aaf986247108af7fa4066d04cd4d55db1b946424
Marcus Willock <crazcalm@gmail.com>
2014-06-01 04:08:26 -0400

    dashboardPopup is now rendering

--- app.py
+++ app.py
@@ -6,9 +6,13 @@ app = flask.Flask(__name__)
 def hello():
     return "hello world"
 
-@app.route("/test")
-def test():
+@app.route("/dashboard")
+def dashboard():
     return flask.render_template("dashboard.html")
+
+@app.route("/dashboardPopup")
+def dashboardPopUp():
+    return flask.render_template("dashboardPopUp.html")
 
 if __name__ == "__main__":
     app.debug = True
