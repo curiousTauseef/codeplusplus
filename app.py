ad65c0ba113e4736a5ce56b0a6a1cff273e8a027
Marcus Willock <crazcalm@gmail.com>
2014-06-01 04:14:33 -0400

    splash is now rendering

--- app.py
+++ app.py
@@ -14,6 +14,10 @@ def dashboard():
 def dashboardPopUp():
     return flask.render_template("dashboardPopUp.html")
 
+@app.route("/login")
+def login():
+    return flask.render_template("splash.html")
+
 if __name__ == "__main__":
     app.debug = True
     app.run()
