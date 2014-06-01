5903ee7dbc85faa846d119323b138d2428e7a615
Marcus Willock <crazcalm@gmail.com>
2014-06-01 12:08:49 -0400

    have a login function

--- classes/csv_reader.py
+++ classes/csv_reader.py
@@ -1,5 +1,14 @@
 import csv
 
+
+def login(username, password):
+    with open("../csv_files/users.csv") as f:
+        f_csv = csv.DictReader(f)
+        for row in f_csv:
+            if row["username"] == username and row["password"] == password:
+                return "You can login"
+        return "Not a user"
+
 def csv_reader(file):
     with open(file) as f:
         f_csv = csv.DictReader(f)
@@ -10,3 +19,9 @@ if __name__ == "__main__":
     csv_reader("../csv_files/users.csv")
     print "\n\n"
     csv_reader("../csv_files/goals.csv")
+
+    print "\n\n"
+    tests = [("crazcalm", "pass1234"), ("crazcalm", "none")]
+    for username, password in tests:
+        print username, password
+        print login(username, password)
